caff029adc467e8c80168b1504a85c1cf7dec7a5
Aluno_Noite <l@ada>
2025-04-03 18:34:11 -0300

    git commit

--- src/main/java/ExercicioJava/CvsProduto.java
+++ src/main/java/ExercicioJava/CvsProduto.java
@@ -1,4 +1,26 @@
 package ExercicioJava;
 
 public class CvsProduto {
+    private String nome;
+    private Double precoUnitario;
+    private Integer quantidade;
+
+    public CvsProduto() {
+    }
+
+    public CvsProduto(String nome, Double precoUnitario, Integer quantidade) {
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularValorTotal(){
+        return precoUnitario * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return nome + "," +  calcularValorTotal();
+    }
+
 }
--- src/main/java/ExercicioJava/Program.java
+++ src/main/java/ExercicioJava/Program.java
@@ -1,4 +1,46 @@
 package ExercicioJava;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        List<CvsProduto> list = new ArrayList<>();
+        list.add(new CvsProduto("Macarrao", 15.99, 15));
+        list.add(new CvsProduto("Feijao", 10.99, 10));
+
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
+
+
+        File sourceFile = new File(sourceFileStr);
+        String strPath = sourceFile.getParent();
+
+        File outDir = new File(strPath + "\\out");
+        if (!outDir.exists()) {
+            boolean success = outDir.mkdir();
+            System.out.println("Directory created successfully: " + success);
+        }
+
+        String targetFileStr = strPath + "\\out\\sumarry.csv";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr, true))) {
+
+            for (CvsProduto produto : list) {
+                bw.write(Arrays.toString(produto.toString().split(",")));
+                bw.newLine();
+            }
+
+            System.out.println("Produtos adicionados ao arquivo CSV.");
+
+        } catch (IOException e) {
+            System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+        }
+    }
 }
